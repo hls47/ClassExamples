fef71f2def556d5d89e7a318188b412a2e44233b
Jay Palat <palatv@upmc.edu>
2014-02-12 23:10:18 -0500

    Initial Commit for Feb13Examples

---
+++ Feb13Class/Feb13Class/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Feb13Class
+//
+//  Created by Build User on 2/12/14.
+//  Copyright (c) 2014 Pitt. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ Feb13Class/Feb13Class/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Feb13Class
+//
+//  Created by Build User on 2/12/14.
+//  Copyright (c) 2014 Pitt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
