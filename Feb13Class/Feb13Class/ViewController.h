f560e44265e51a43ee78a7c008767aea478f8d53
Jay Palat <palatv@upmc.edu>
2014-02-14 10:11:27 -0500

    Example from in class

--- Feb13Class/Feb13Class/ViewController.h
+++ Feb13Class/Feb13Class/ViewController.h
@@ -8,6 +8,15 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController <NSURLConnectionDelegate, NSURLConnectionDataDelegate, UITextFieldDelegate>
+
+
+@property (nonatomic, weak) IBOutlet UITextField *urlField;
+@property (nonatomic, weak) IBOutlet UITextView *urlContent;
+@property (nonatomic, weak) IBOutlet UIButton *goButton;
+
+@property (nonatomic, strong) NSMutableString *responseString;
+
+-(IBAction)loadURL:(id)sender;
 
 @end
--- Feb13Class/Feb13Class/ViewController.m
+++ Feb13Class/Feb13Class/ViewController.m
@@ -18,12 +18,68 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    _responseString = [[NSMutableString alloc] initWithCapacity:0];
+
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
+    
     // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)loadURL:(id)sender
+{
+    [_urlField resignFirstResponder];
+    _responseString = nil;
+    _responseString = [[NSMutableString alloc] initWithCapacity:0];
+    NSString *simpleAddr =[_urlField text];
+    NSLog(@"simpleAddr: %@", simpleAddr);
+    
+    if (simpleAddr == nil || [simpleAddr isEqualToString:@""] ) {
+        
+        simpleAddr = @"http://www.pitt.edu";
+    }
+    
+    NSURL *url = [NSURL URLWithString:simpleAddr];
+    NSURLRequest *request = [NSURLRequest requestWithURL:url];
+    
+    NSURLConnection * conn = [NSURLConnection connectionWithRequest:request delegate:self];
+    
+    if (conn!=nil){
+        [conn start];
+    }
+}
+
+#pragma mark - UITextField Delegate
+
+-(BOOL)textFieldShouldReturn:(UITextField *)textField{
+    [textField resignFirstResponder];
+    return YES;
+}
+
+#pragma mark - NSURLConnectionDelegate
+
+-(void)connectionDidFinishLoading:(NSURLConnection *)connection{
+    UIAlertView *uiAlert = [[UIAlertView alloc] initWithTitle:@"Done!" message:@"Data is here!" delegate:self cancelButtonTitle:@"ok" otherButtonTitles: nil];
+    [uiAlert show];
+    
+    [self.urlContent setContentOffset:CGPointMake(0, 0) animated:YES];
+    
+    [_urlContent setText:_responseString];
+}
+
+-(void)connection:(NSURLConnection *)connection didReceiveData:(NSData *)data{
+    NSString *newData = [NSString stringWithUTF8String:[data bytes]];
+    
+    [_responseString appendString:newData];
+}
+
+-(void)connection:(NSURLConnection *)connection didFailWithError:(NSError *)error{
+    UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Error" message:[error debugDescription] delegate:self cancelButtonTitle:@"Acknowledged" otherButtonTitles: nil];
+    [alert show];
 }
 
 @end
